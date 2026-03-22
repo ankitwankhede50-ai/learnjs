a9587d50895a7c64fd7ae8ec684631271778ea53
ankitwankhede50-ai <ankitwankhede50@gmail.com>
2026-03-22 13:07:18 +0000

    basics variables ,datatypes learnt

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strtict"; // treat all js code as newer version
+
+//alert(3+3) // we are using node not browser 
+
+let name="Ankit"
+let age=21
+let isloggedIn =false
+let state;
+
+console.log(typeof undefined)//undefined is a datatype 
+console.log(typeof null) // null is treated as a
