33617426810bffd6288be634d9d266d777045825
ankitwankhede50-ai <ankitwankhede50@gmail.com>
2026-03-23 12:44:07 +0000

    nums and maths

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,20 @@
+const name ="ankit"
+const repoCount = 50 
+
+//console.log(name + repoCount )
+
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`)
+
+
+const gameName = new String('hiteshhc')
+
+console.log(gameName[0])
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('c'))
+
+const newString =gameName.substring(0,4)
+console.log(newString)
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString)
--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -5,7 +5,7 @@
 const id =Symbol('123')
 const anotherid = Symbol('123')
 
-console.log(id==anotherid)
+//console.log(id==anotherid)
 
 //reference type (non primitive) call by reference
 
@@ -23,4 +23,30 @@ const myFunction = function (){
     console.log("hello world");
 }
 
-console.log(typeof myFunction)
+//console.log(typeof myFunction)
+
+
+
+
+//stack(primitve), Heap (non primitive)
+
+let myYtname = "ankitwankhede"
+
+let anotherName = myYtname 
+
+anotherName= "chaiWithCode";
+
+console.log(anotherName)
+console.log(myYtname)
+
+let user1 = {
+    email :"user12@gmail.com",
+    upi : "1234@ybl"
+}
+
+let user2 = user1 
+
+user2.email="user2@gmail.com"
+
+console.log(user1.email)
+console.log(user2.email)
